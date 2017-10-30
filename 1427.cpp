25650e6fcef42f6faba8437633d53438f3a7ede3
“LimJinGyu” <leemjg92@naver.com>
2017-10-30 14:31:24 +0900

    소트인사이드(정렬)

--- 1427.cpp
+++ 1427.cpp
@@ -1,22 +1,22 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//	long long N;
-//	cin >> N;
-//
-//	int arr[10] = { 0, };
-//	while (N != 0) {
-//		arr[N % 10]++;
-//		N /= 10;
-//	}
-//	
-//	for(int i = 9; i>=0; i--) {
-//		while (arr[i] != 0) {
-//			cout << i;
-//			arr[i]--;
-//		}
-//	}
-//
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long N;
+	cin >> N;
+
+	int arr[10] = { 0, };
+	while (N != 0) {
+		arr[N % 10]++;
+		N /= 10;
+	}
+	
+	for(int i = 9; i>=0; i--) {
+		while (arr[i] != 0) {
+			cout << i;
+			arr[i]--;
+		}
+	}
+
+	return 0;
+}
