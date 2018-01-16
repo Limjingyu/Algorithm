cea10d3893ff71135aff6322f9ff215cf3581a1f
“LimJinGyu” <leemjg92@naver.com>
2018-01-17 00:12:56 +0900

    숨바꼭질(BFS)

---
+++ 1697.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+bool visited[100001];
+
+int main(void) {
+	int N, K;
+	cin >> N >> K;
+
+	int n, cnt;
+	queue<int> q_n, q_cnt;
+	q_n.push(N); q_cnt.push(0);
+	while (!q_n.empty()) {
+		n = q_n.front();
+		cnt = q_cnt.front();
+		q_n.pop();
+		q_cnt.pop();
+		if (n < 0 || n > 100000) continue;
+		if (visited[n]) continue;
+		visited[n] = true;
+		if (n == K) break;
+		q_n.push(n * 2); q_cnt.push(cnt + 1);
+		q_n.push(n + 1); q_cnt.push(cnt + 1);
+		q_n.push(n - 1); q_cnt.push(cnt + 1);
+	}
+
+	cout << cnt << endl;
+	return 0;
+}
