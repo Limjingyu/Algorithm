e373d593b0a7c8d337e939e8b6892f726bd18227
“LimJinGyu” <leemjg92@naver.com>
2018-09-18 23:13:06 +0900

    피보나치 함수(DP)

--- 1003.cpp
+++ 1003.cpp
@@ -1,31 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int num0, num1;
+int dp[42][2];
 
-int fibonacci(int n) {
-	if (n == 0) {
-		num0++;
-		return 0;
-	}
-	else if (n == 1) {
-		num1++;
-		return 1;
-	}
-	else {
-		return (fibonacci(n - 1) + fibonacci(n - 2));
+void setting() {
+	dp[0][0] = 1;
+	dp[0][1] = 0;
+	dp[1][0] = 0;
+	dp[1][1] = 1;
+
+	for (int i = 2; i <= 40; i++) {
+		dp[i][0] = dp[i - 2][0] + dp[i - 1][0];
+		dp[i][1] = dp[i - 2][1] + dp[i - 1][1];
 	}
 }
 
 int main() {
 	int T, N;
 	cin >> T;
-
+	setting();
 	for (int i = 0; i < T; i++) {
 		cin >> N;
-		num0 = num1 = 0;
-		fibonacci(N);
-		cout << num0 << " " << num1 << endl;
+		cout << dp[N][0] << " " << dp[N][1] << endl;
 	}
 	return 0;
 }
