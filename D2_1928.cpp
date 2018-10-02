61619ef1b72d84dfa5c7b62d6d65433cc0873e07
“LimJinGyu” <leemjg92@naver.com>
2018-10-03 01:31:30 +0900

    Base64 Decoder

---
+++ D2_1928.cpp
@@ -0,0 +1,45 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int changeChartoInt(char c) {
+//	if ('A' <= c && c <= 'Z')
+//		return c - 'A';
+//	else if ('a' <= c && c <= 'z')
+//		return c - 'a' + 26;
+//	else if ('0' <= c && c <= '9')
+//		return c - '0' + 52;
+//	else if (c == '+')
+//		return 62;
+//	else
+//		return 63;
+//}
+//string process() {
+//	string input;
+//	cin >> input;
+//	
+//	int len = input.length();
+//	string sol;
+//
+//	for (int i = 0; i < len; i += 4) {
+//		int ch[4] = { 0, };
+//		for (int j = i; j < i + 4; j++) {
+//			ch[j - i] = changeChartoInt(input[j]);
+//		}
+//		sol += (char)(ch[0] << 2) + (char)(ch[1] >> 4);
+//		sol += (char)(ch[1] << 4) + (char)(ch[2] >> 2);
+//		sol += (char)(ch[2] << 6) + (char)(ch[3]);
+//	}
+//
+//	return sol;
+//}
+//int main() {
+//	int N;
+//	cin >> N;
+//
+//	for (int i = 1; i <= N; i++) {
+//		cout << "#" << i << " " << process() << endl;
+//	}
+//
+//	return 0;
+//}
